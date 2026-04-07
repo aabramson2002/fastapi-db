ec91b4a04ed445f48e10cab220bef25ffe2b354a
aabramson2002 <aabramson44@comcast.net>
2026-04-07 08:22:33 -0400

    Added database url to config to prevent authentication failiure

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@localhost:5432/fastapi_db"
+    )
     
     class Config:
         env_file = ".env"
